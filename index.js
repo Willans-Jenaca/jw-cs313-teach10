bae40940c7f20058cb0d7c2033c0807486d1a094
Willans-Jenaca <princesspoofball@yahoo.com>
2018-03-11 01:22:37 -0800

    db

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const app = express();
 
 var pg = require('pg');
 
+// var pool = new pg.Pool()
+
+
+// app.get('/db', function (request, response) {
+//   pool.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM node.person', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('views/pages/db', {results: result.rows} ); }
+//     });
+//   });
+// });
+
+// // pool shutdown
+// pool.end()
+
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM node.person', function(err, result) {
@@ -11,7 +29,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('views/pages/db', {results: result.rows} ); }
     });
   });
 });
