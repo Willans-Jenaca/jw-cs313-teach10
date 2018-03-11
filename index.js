152073d47f1cb8822a9d22ae81b5c162220aee9b
Willans-Jenaca <princesspoofball@yahoo.com>
2018-03-11 00:04:46 -0800

    teach10 1

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
 const express = require('express');
 const app = express();
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM node.person', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 // Following example from https://github.com/heroku/node-js-getting-started/
 // This allows us to run it locally or remotely on Heroku
 const PORT = process.env.PORT || 5000;
 
-// operand map
-const OP_FUNC = {
-  add: add,
-  sub: sub,
-  mul: mul,
-  div: div,
-};
 
-// op symbol map
-const OP_SYMB = {
-  add: '+',
-  sub: '-',
-  mul: 'x',
-  div: '/',
-};
 
 app.use(express.static('public'));
 
@@ -30,46 +30,7 @@ app.set('view engine', 'ejs');
 // And here's our home page
 app.get('/', (req, res) => res.render('home'));
 
-// Our math page
-app.get('/math', (req, res) => {
-  let result = OP_FUNC[req.query.operator](+req.query.op1, +req.query.op2);
-  console.log(result);
-  res.render('result', {
-    op1: +req.query.op1,
-    op2: +req.query.op2,
-    op: OP_SYMB[req.query.operator],
-    result: result,
-  });
-});
 
-// Our math service
-app.get('/math_service/:op/:op1/:op2', (req, res) => {
-  console.log(req.params);
-  let result = OP_FUNC[req.params.op](+req.params.op1, +req.params.op2);
-  console.log(result);
-  res.json({
-    op1: +req.params.op1,
-    op2: +req.params.op2,
-    op: OP_SYMB[req.params.op],
-    result: result,
-  });
-});
-
-function add(op1, op2) {
-  return op1 + op2;
-}
-
-function sub(op1, op2) {
-  return op1 - op2;
-}
-
-function mul(op1, op2) {
-  return op1 * op2;
-}
-
-function div(op1, op2) {
-  return op1 / op2;
-}
 
 // Let's start listening
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
