8988e0ba1f3be39633ee40875a02d8d74c86b23c
Willans-Jenaca <princesspoofball@yahoo.com>
2018-03-12 20:20:55 -0700

    teacher

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-const express = require('express');
-const app = express();
 
-var pg = require('pg');
+var express = require('express');
+var app = express();
 
-// And here's our home page
- app.get('/', (req, res) => res.render('home'));
-//app.get('/', (req, res) => res.send('Hello World'));
-
-
-app.get('/getPerson', function(request, response) {
-  getPerson(request, response);
-});
-
-
-// query to select all from database
-//   app.get('/getPerson', function (request, response) {
-//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//       client.query('SELECT * FROM node.person', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        { response.render('views/getPerson', {results: result.rows} ); }
-//     });
-//   });
-// });
+var pg = require("pg"); // This is the postgres database connection module.
+const connectionString = "postgres://testuser:testuser@localhost:5432/'Jenaca'";
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -34,39 +13,69 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/getPerson', function(request, response) {
+  getPerson(request, response);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 function getPerson(request, response) {
-  //response.send("getPerson arrived");
+  // First get the person's id
   var id = request.query.id;
-  // getPersonFromDb(id, function(error, result) {
-   // getPersonFromDb(id {
-    getPersonFromDb(id, response);
-  //   if (error || result == null || result.length != 1) {
-  //     response.status(500).json({success: false, data: error});
-  //   } else {
-  //     var person = result[0];
-  //     response.status(200).json(result[0]);
-  //   }
-  // });
+
+  // TODO: It would be nice to check here for a valid id before continuing on...
+
+  // use a helper function to query the DB, and provide a callback for when it's done
+  getPersonFromDb(id, function(error, result) {
+    // This is the callback function that will be called when the DB is done.
+    // The job here is just to send it back.
+
+    // Make sure we got a row with the person, then prepare JSON to send back
+    if (error || result == null || result.length != 1) {
+      response.status(500).json({success: false, data: error});
+    } else {
+      var person = result[0];
+      response.status(200).json(result[0]);
+    }
+  });
 }
 
-// function getPersonFromDb(id, callback) {
-  function getPersonFromDb(id, response) {
+function getPersonFromDb(id, callback) {
   console.log("Getting person from DB with id: " + id);
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-       client.query('SELECT * FROM node.person', function(err, result) {
-       done();
-        if (err)
-         { console.error(err); response.send("Error " + err); }
-        else
-         { response.render('views/result', {results: result.rows} ); }
+  var client = new pg.Client(connectionString);
+
+  client.connect(function(err) {
+    if (err) {
+      console.log("Error connecting to DB: ")
+      console.log(err);
+      callback(err, null);
+    }
+
+    // var sql = "SELECT id, firstname, lastname, birthdate FROM node.person WHERE id = $1::int";
+    var sql = "SELECT * FROM node.person";
+    var params = [id];
+
+    var query = client.query(sql, params, function(err, result) {
+      // we are now done getting the data from the DB, disconnect the client
+      client.end(function(err) {
+        if (err) throw err;
       });
+
+      if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+        callback(err, null);
+      }
+
+      console.log("Found result: " + JSON.stringify(result.rows));
+
+      // call whatever function the person that called us wanted, giving it
+      // the results that we have been compiling
+      callback(null, result.rows);
+    });
   });
-    
 
 } // end of getPersonFromDb
-
---
+++ testindex.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+
+var pg = require('pg');
+
+// And here's our home page
+ app.get('/', (req, res) => res.render('home'));
+//app.get('/', (req, res) => res.send('Hello World'));
+
+
+app.get('/getPerson', function(request, response) {
+  getPerson(request, response);
+});
+
+
+// query to select all from database
+//   app.get('/getPerson', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//       client.query('SELECT * FROM node.person', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('views/getPerson', {results: result.rows} ); }
+//     });
+//   });
+// });
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+function getPerson(request, response) {
+  //response.send("getPerson arrived");
+  var id = request.query.id;
+  // getPersonFromDb(id, function(error, result) {
+    getPersonFromDb(id, response);
+  //   if (error || result == null || result.length != 1) {
+  //     response.status(500).json({success: false, data: error});
+  //   } else {
+  //     var person = result[0];
+  //     response.status(200).json(result[0]);
+  //   }
+  // });
+}
+
+// function getPersonFromDb(id, callback) {
+  function getPersonFromDb(id, response) {
+  console.log("Getting person from DB with id: " + id);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+       // client.query('SELECT * FROM node.person', function(err, result) {
+        client.query('SELECT now()', function(err, result) {
+       done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.render('views/result', {results: result.rows} ); }
+      });
+  });
+    
+
+} // end of getPersonFromDb
+
