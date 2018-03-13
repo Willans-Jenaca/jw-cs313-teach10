a4740c4ba56d8ba51bdc7254a29b85b05c9d1df3
Willans-Jenaca <princesspoofball@yahoo.com>
2018-03-12 19:23:36 -0700

    t :

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function getPersonFromDb(id, callback) {
   console.log("Getting person from DB with id: " + id);
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM node.person WHERE', function(err, result) {
+      client.query('SELECT * FROM node.person', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
